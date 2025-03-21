ec9255ad5a9e8c968e6876ae8e5e08f58180d300
FernandoQuiGuz <faquilaqueo@miuandes.cl>
2025-03-21 18:40:31 -0300

    lab 2.3

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 async function getFetch() {
-    const apiKey = 'a356b91066c84c5eab68b3d3e73a81a8'; 
-    const category = 'technology'; 
-    const url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${apiKey}&pageSize=10`;
+    const url = `https://newsapi.org/v2/top-headlines?category=sports&apiKey=a356b91066c84c5eab68b3d3e73a81a8&pageSize=10`;
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-        if (data.status === "ok") {
-            displayArticles(data.articles);
-        } else {
-            console.error("Error en la respuesta de la API", data);
-        }
-    } catch (error) {
-        console.error("Error al obtener los datos:", error);
-    }
+    displayArticles(data.articles);
 }
+
 function displayArticles(articles) {
     const container = document.getElementById("data_container");
     container.innerHTML = ""; 
-    const list = document.createElement("ul"); 
+
     articles.forEach(article => {
-        const listItem = document.createElement("li"); 
-        listItem.innerHTML = `
-            <span>${article.title}</span><br>
-            <span style="color: gray; font-size: 12px;">${article.source.name}</span><br>
-            <p style="font-size: 12px; margin-top: 5px;">${article.description || "No description available"}</p>
+        const card = document.createElement("div");
+        card.classList.add("card", "mb-3"); 
+
+        card.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${article.title}</h5>
+                <p class="card-text">${article.description || "No description available"}</p>
+                <a href="${article.url}" class="btn btn-primary" target="_blank">See More</a>
+            </div>
         `;
-        list.appendChild(listItem); 
+
+        container.appendChild(card);
     });
-    container.appendChild(list);
 }
+
 document.addEventListener("DOMContentLoaded", getFetch);
